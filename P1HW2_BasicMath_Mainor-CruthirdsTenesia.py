47c521d78092ab4901a635e836bb8971ca655b2c
mainorct3328 <70215545+mainorct3328@users.noreply.github.com>
2020-09-15 10:42:53 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_Mainor-CruthirdsTenesia.py
@@ -0,0 +1,20 @@
+#This project is to show how IDLE is used when displaying basic math and inputs.
+#September 14, 2020
+#CTI-110 P1HW2 - Basic Math
+#Tenesia Mainor-Cruthirds
+
+first_number = 7
+second_number = 5
+
+print('Please enter your first number:',first_number)
+print('Please enter your second number:',second_number)
+print("User's first number entered:", first_number)
+print("User's second number entered:", second_number)
+
+print("The sum of User's two numbers",first_number + second_number)
+print("The result of multiplying User's two numbers",first_number * second_number)
+
+
+#The input was to get two numbers from User.
+#The Process was to add and multiply the two numbers.
+#The output were results of addition and multiplication from User's two numbers. 
